7520c59235cf748c3ba7c7b306cdf3ebc37b499b
Kala Elam <speechgirl1505@gmail.com>
2019-10-15 13:54:24 -0500

    not a bad start

--- app.js
+++ app.js
@@ -2,16 +2,48 @@ $(document).ready(function () {
 
     $(document).on("click", ".hideMeNext", function () {
         event.preventDefault();
-        $(".hideMe").show();
+        $(".allAboutMe").show();
         $(".hideMeNext").hide();
+        $(".workPort").hide();
 
       });
+
       $(document).on("click", ".portal", function () {
         event.preventDefault();
-        $(".work").show();
         $(".hideMeNext").hide();
-        $(".hideMe").hide();
+        $(".work").show();
+        $(".workPort").show();
+        $(".allAboutMe").hide();
+        $(".hitMeUp").hide();
 
       });
+
+      $(document).on("click", ".contact", function () {
+        event.preventDefault();
+        $(".hideMeNext").hide();
+        $(".hitMeUp").show();
+        $(".work").hide();
+        $(".workPort").hide();
+        $(".allAboutMe").hide();
+
+      });
+     
+      $(document).on("click", ".about", function () {
+        event.preventDefault();
+        $(".hideMeNext").hide();
+        $(".allAboutMe").show();
+        $(".work").hide();
+        $(".workPort").hide();
+        $(".hitMeUp").hide();
+    
+      });
+
+      // $(document).on("click", ".", function () {
+      //   event.preventDefault();
+      //   $(".allAboutMe").hide();
+      //   $(".hideMeNext").hide();
+      //   $(".hitMeUp").hide();
+    
+      // });
 
     });
